e77975f9d119c4412af932bb7abf27b573de3bb4
Aguel Satria Wijaya <30289344+LERUfic@users.noreply.github.com>
2017-11-15 14:19:53 +0700

    Need Debug

--- soal4.c
+++ soal4.c
@@ -7,38 +7,52 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
-#include <sys/stat.h>
+#include <stdlib.h>
 
-static const char *default_dir = "/home/praktikum/Downloads";
+static const char *dirpath = "/home/falnerz/Downloads";
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
 	int res;
-    char new_path[256];
-    sprintf(new_path,"%s%s",default_dir,path);
-	res = lstat(new_path, stbuf);
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
+
 	if (res == -1)
 		return -errno;
 
 	return 0;
 }
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,off_t offset, struct fuse_file_info *fi)
+static int xmp_chmod(const char *path, mode_t mode)
 {
-    DIR *dp;
+ int res;
+ char fpath[1000];
+ sprintf(fpath,"%s%s",dirpath,path);
+    res = chmod(fpath, mode);
+    if (res == -1)
+        return -errno;
+    return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+	char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+
+	DIR *dp;
 	struct dirent *de;
-    char new_path[256];
-    if( !strcmp(path,"/") ){
-        path=default_dir;
-//        sprintf(new_path,"%s",);
-        strcpy(new_path,default_dir);
-    }
-    else sprintf(new_path,"%s%s",default_dir,path);
 
 	(void) offset;
 	(void) fi;
 
-	dp = opendir(new_path);
+	dp = opendir(fpath);
 	if (dp == NULL)
 		return -errno;
 
@@ -47,57 +61,160 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,off_t
 		memset(&st, 0, sizeof(st));
 		st.st_ino = de->d_ino;
 		st.st_mode = de->d_type << 12;
-		if (filler(buf, de->d_name, &st, 0))
-			break;
+		res = (filler(buf, de->d_name, &st, 0));
+		if(res!=0) break;
 	}
 
 	closedir(dp);
 	return 0;
 }
-/*
-*/
 
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
- 	int fd;
-	int res;
-	char new_path[256];
+	char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	
+	int res = 0;
+	int fd = 0 ;
+
 	(void) fi;
-
-    sprintf(new_path,"%s%s",default_dir,path);
-
-	printf("%s\n\n", new_path);
-    fd = open(new_path, O_RDONLY);
+	fd = open(fpath, O_RDONLY);
 	if (fd == -1){
 		return -errno;
 	}
 	else{
-        char ext[256];
-	int ext_idx;
+	    char ext[256];
 
-        int u;
-        for(u = 0; u < strlen(new_path) && new_path[u] != '.'; u++);
-	strcpy(ext, new_path+u);
-        for(ext_idx=0;ext_idx<1;++ext_idx){
-            if(!strcmp(ext,".copy")){
-                 system("zenity --error --title 'Error' --text 'File yang anda buka adalah file hasil salinan. File tidak bisa diubah maupun disalin kembali!'");
-		return ;
-            }
-	 else{
-	res = pread(fd, buf, size, offset);
-}
-        }
-        if (res == -1)res = -errno;
-        close(fd);
+	    int u;
+	    for(u = strlen(fpath)-1; u>-1 && fpath[u] != '.'; u--);
+	    strcpy(ext,fpath+u);
+
+	    if( !strcmp(ext,".copy") ){
+	        system("zenity --error --title 'Error' --text 'File yang anda buka adalah file hasil salinan. File tidak bisa diubah maupun disalin kembali!'");
+	        return -errno;
+	    }
+
+	    res = pread(fd, buf, size, offset);
+	    if (res == -1)
+	        res = -errno;
+
+	    close(fd);
+	    return res;
 	}
+}
+
+static int xmp_rename(const char *from, const char *to)
+{
+	//system("zenity --error --title 'Error' --text 'Terjadi Kesalahan! File berisi konten berbahaya.'");
+	int res;
+	char new_from[1000];
+	char new_to[1000];
+	system("mkdir /home/falnerz/Downloads/simpanan -p");
+	char direktori[] = "/home/falnerz/Downloads/simpanan";
+	sprintf(new_from,"%s%s",dirpath,from);
+	sprintf(new_to,"%s%s.copy",direktori,to);
+	res = rename(new_from, new_to);
+	char command[1000];
+	sprintf(command,"cp %s %s",new_from,new_to);
+	system(command);
+
+	if(res == -1)
+	 return -errno;
+
+	return 0;
+}
+
+static int xmp_truncate(const char *path, off_t size)
+{
+	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s", dirpath, path);
+
+	res = truncate(fpath, size);
+	if(res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s", dirpath, path);
+	(void) fi;
+	fd = open(fpath, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
 	return res;
 }
 
+static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s", dirpath, path);
+	res = mknod(fpath, mode, rdev);
+	if(res == -1)
+	    return -errno;
+
+	return 0;
+	}
+
+
+
+static int xmp_open(const char *path, struct fuse_file_info *fi)
+{
+	char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+
+	int res;
+	res = open(fpath, fi->flags);
+	if (res == -1)
+		return -errno;
+
+	close(res);
+	return 0;
+}
+
+
+static int xmp_utimens(const char *path, const struct timespec ts[2])
+{
+	int res;
+	/* don't use utime/utimes since they follow symlinks */
+	res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
+	if (res == -1)return -errno;
+	return 0;
+}
+
 static struct fuse_operations xmp_oper = {
-	.getattr	= xmp_getattr,
-	.readdir	= xmp_readdir,
-	.read		= xmp_read,
+	.getattr 	= xmp_getattr,
+	.readdir 	= xmp_readdir,
+	.read 		= xmp_read,
+	.rename 	= xmp_rename,
+	.write 		= xmp_write,
+	.mknod 		= xmp_mknod,
+	.truncate 	= xmp_truncate,
+	.open 		= xmp_open,
+	.chmod 		= xmp_chmod,
+	.utimens 	= xmp_utimens,
 };
 
 int main(int argc, char *argv[])
